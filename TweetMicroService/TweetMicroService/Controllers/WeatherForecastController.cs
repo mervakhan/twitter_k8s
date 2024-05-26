368926499401cca214afd82e5b8a3c5f818c0b7d
i450024 <i450024@fhict.nl>
2024-05-26 20:26:19 +0200

    lets update to see

--- TweetMicroService/TweetMicroService/Controllers/WeatherForecastController.cs
+++ TweetMicroService/TweetMicroService/Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace TweetMicroService.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Summary = "Summ "+ Summaries[Random.Shared.Next(Summaries.Length)],
                 UserId = UserId
             })
             .ToArray();
