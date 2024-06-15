0e3f0bd485d823ec6ff4205a343da93b6f184672
i450024 <mervakhan0786@gmail.com>
2024-06-15 13:29:57 +0200

    db errro

--- TweetMicroService/AuthenticationMicroService/DbContexts/UserContext.cs
+++ TweetMicroService/AuthenticationMicroService/DbContexts/UserContext.cs
@@ -8,5 +8,12 @@ namespace AuthenticationMicroService.DbContexts
         public UserContext(DbContextOptions<UserContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.ID); // Specify the primary key
+                                        // Additional configuration if needed
+        }
     }
 }
